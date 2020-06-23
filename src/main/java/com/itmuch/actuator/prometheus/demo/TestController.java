a698f720b555c942d151db54db93f9d3d51cf768
zhaohq <zhaohq@fastmail.com>
2020-06-23 10:46:36 +0800

    update

--- src/main/java/com/itmuch/actuator/prometheus/demo/TestController.java
+++ src/main/java/com/itmuch/actuator/prometheus/demo/TestController.java
@@ -1,15 +1,24 @@
 package com.itmuch.actuator.prometheus.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author itmuch.com
  */
+
 @RestController
 public class TestController {
+
+    Logger logger=LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/test")
     public String test() {
+        logger.debug("this is debug log");
+        logger.info("this is info log");
+
         return "test";
     }
 }
