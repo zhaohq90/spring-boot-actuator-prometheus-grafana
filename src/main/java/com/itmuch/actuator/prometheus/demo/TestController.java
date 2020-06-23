dfd0ae507169f22cb7dcb465ba7d835a0968fabf
zhaohq <zhaohq@fastmail.com>
2020-06-23 15:23:59 +0800

    update

--- src/main/java/com/itmuch/actuator/prometheus/demo/TestController.java
+++ src/main/java/com/itmuch/actuator/prometheus/demo/TestController.java
@@ -3,11 +3,8 @@ package com.itmuch.actuator.prometheus.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author itmuch.com
- */
 
 @RestController
 public class TestController {
@@ -15,10 +12,15 @@ public class TestController {
     Logger logger=LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/test")
-    public String test() {
+    public String test(@RequestParam(required = false) String name) {
         logger.debug("this is debug log");
         logger.info("this is info log");
 
+        if("admin".equals(name)){
+            throw new RuntimeException("err name");
+        }
+
         return "test";
     }
+
 }
